88e59806d3d8bd3180dcde83401467ac464518db
Shaun Patterson <shaunpatterson@gmail.com>
2016-03-06 17:58:35 -0500

    Nunjucks compiled test

--- render.js
+++ render.js
@@ -2,34 +2,41 @@ var nunjucks = require('nunjucks');
 var swig = require('swig');
 
 var data = {
-  header: 'Colors',
-  items: [{
-    name: 'red',
-    current: true,
-    url: '#Red'
-  }, {
-    name: 'green',
-    current: false,
-    url: '#Green'
-  }, {
-    name: 'blue',
-    current: false,
-    url: '#Blue'
-  }]
+    header: 'Colors',
+    items: [{
+        name: 'red',
+        current: true,
+        url: '#Red'
+    }, {
+        name: 'green',
+        current: false,
+        url: '#Green'
+    }, {
+        name: 'blue',
+        current: false,
+        url: '#Blue'
+    }]
 };
 
+var template = nunjucks.compile('simple.html');
+
 exports.compare = {
-  swig: doSwig,
-  nunjucks: doNunjucks
+    swig: doSwig,
+    nunjucks: doNunjucks,
+    nunjucksCompiled: doNunjucksCompiled
 };
 
 function doNunjucks(done) {
-  nunjucks.render('simple.html', data, done);
+    nunjucks.render('simple.html', data, done);
+}
+
+function doNunjucksCompiled(done) {
+    template.render(data, done);
 }
 
 swig.setDefaults({ cache: 'memory' });
 function doSwig(done) {
-  swig.renderFile('simple.html', data, done);
+    swig.renderFile('simple.html', data, done);
 }
 
 require('bench').runMain();
